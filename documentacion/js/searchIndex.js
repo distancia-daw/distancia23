56749d108dd14f13b4c41ac82a80d06c9a774232
distancia-daw <134981091+distancia-daw@users.noreply.github.com>
2023-05-30 02:42:20 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020para\u0020sumar\u0020dos\u0020n\u00FAmeros",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\restar\u0028\u0029",
+            "name": "restar",
+            "summary": "Funci\u00F3n\u0020para\u0020restar\u0020dos\u0020n\u00FAmeros",
+            "url": "namespaces/default.html#function_restar"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora.",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003AcalcularPromedio\u0028\u0029",
+            "name": "calcularPromedio",
+            "summary": "Calcula\u0020el\u0020promedio\u0020de\u0020un\u0020conjunto\u0020de\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_calcularPromedio"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003AobtenerFactorial\u0028\u0029",
+            "name": "obtenerFactorial",
+            "summary": "Calcula\u0020el\u0020factorial\u0020de\u0020un\u0020n\u00FAmero.",
+            "url": "classes/Calculadora.html#method_obtenerFactorial"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
